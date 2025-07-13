adbae45235f9cc98533cecb61aa1ee34fcb0e2fa
bhavika <bhavikabajaj82@gmail.com>
2025-07-13 09:58:29 +0530

    Adding if for conditional rendering

--- learning-fragments/src/App.jsx
+++ learning-fragments/src/App.jsx
@@ -7,9 +7,12 @@ import React from "react";
 
 function App() {
   let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk"];
+  if (foodItems.length === 0) {
+    return <h3>I am still hungry</h3>;
+  }
   return (
     <>
-      <div>Healthy Foods</div>
+      <h1>Healthy Food</h1>
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
