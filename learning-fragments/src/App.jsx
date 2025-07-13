fbbaff0dec208e4409fd3ab627067788879d1676
bhavika <bhavikabajaj82@gmail.com>
2025-07-13 10:01:05 +0530

    Adding logical operation for conditional rendering

--- learning-fragments/src/App.jsx
+++ learning-fragments/src/App.jsx
@@ -6,13 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
 function App() {
-  // let foodItems = [];
-  let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk"];
-  let emptyMessage = foodItems.length === 0 ? <h3>I am still hungry</h3> : null;
+  let foodItems = [];
+  // let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk"];
   return (
     <>
       <h1>Healthy Food</h1>
-      {emptyMessage}
+      {foodItems.length === 0 && <h3>I am still hungry</h3>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
