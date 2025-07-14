2ec52cf5d67fc6de87988e998845e5113112eef9
bhavika <bhavikabajaj82@gmail.com>
2025-07-14 08:06:57 +0530

    Learning css modules

--- learning-fragments/src/App.jsx
+++ learning-fragments/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk"];
   return (
     <>
-      <h1>Healthy Food</h1>
+      <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage items={foodItems}></ErrorMessage>
       <FoodItems items={foodItems}></FoodItems>
     </>
--- learning-fragments/src/components/Item.jsx
+++ learning-fragments/src/components/Item.jsx
@@ -1,4 +1,10 @@
+import styles from "./Item.module.css";
+console.log(styles);
 const Item = ({ foodItem }) => {
-  return <li className="list-group-item">{foodItem}</li>;
+  return (
+    <li className={`${styles["item"]} list-group-item`}>
+      <span className={styles["item-span"]}>{foodItem}</span>
+    </li>
+  );
 };
 export default Item;
