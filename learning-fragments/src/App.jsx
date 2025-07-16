ca0dcfbefde8963fc6ce0cd595e6129766404f28
bhavika <bhavikabajaj82@gmail.com>
2025-07-16 22:49:07 +0530

    Learning useState hook

--- learning-fragments/src/App.jsx
+++ learning-fragments/src/App.jsx
@@ -4,19 +4,35 @@ import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import Container from "./components/Container";
 import FoodInput from "./components/Foodinput";
+import { useState } from "react";
 
 function App() {
   // let foodItems = [];
-  let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk"];
-  const handleOnChange = (event) => {
-    console.log(event.target.value);
+  // let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk"];
+  let [foodItems, setFoodItems] = useState(["Dal", "Green Vegetables", "Roti"]);
+
+  // let textStateArr = useState("Food item entered by user");
+  // let textToShow = textStateArr[0];
+  // let setTextState = textStateArr[1];
+  let [textToShow, setTextState] = useState();
+  // console.log(`Current value of text State is ${textToShow}`);
+
+  const onKeyDown = (event) => {
+    // console.log(event);
+    if (event.key === "Enter") {
+      let newFoodItem = event.target.value;
+      let newItems = [...foodItems, newFoodItem];
+      setFoodItems(newItems);
+      event.target.value = "";
+    }
   };
   return (
     <>
       <Container>
         <h1 className="food-heading">Healthy Food</h1>
         <ErrorMessage items={foodItems}></ErrorMessage>
-        <FoodInput handleOnChange={handleOnChange}></FoodInput>
+        <FoodInput handleKeyDown={onKeyDown}></FoodInput>
+        <p>{textToShow}</p>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
     </>
--- learning-fragments/src/components/FoodInput.jsx
+++ learning-fragments/src/components/FoodInput.jsx
@@ -1,11 +1,11 @@
 import styles from "./FoodInput.module.css";
-const FoodInput = ({ handleOnChange }) => {
+const FoodInput = ({ handleKeyDown }) => {
   return (
     <input
       type="text"
       placeholder="Enter food item here"
       className={styles["food-input"]}
-      onChange={handleOnChange}
+      onKeyDown={handleKeyDown}
     />
   );
 };
