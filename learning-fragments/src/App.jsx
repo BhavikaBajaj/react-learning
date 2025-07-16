a5cece1e410a756fbc7b56b00c7f41e0411d9d95
bhavika <bhavikabajaj82@gmail.com>
2025-07-16 08:01:21 +0530

    Learning: passing functions as props

--- learning-fragments/src/App.jsx
+++ learning-fragments/src/App.jsx
@@ -8,17 +8,17 @@ import FoodInput from "./components/Foodinput";
 function App() {
   // let foodItems = [];
   let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk"];
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <>
       <Container>
         <h1 className="food-heading">Healthy Food</h1>
         <ErrorMessage items={foodItems}></ErrorMessage>
-        <FoodInput></FoodInput>
+        <FoodInput handleOnChange={handleOnChange}></FoodInput>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
-      {/* <Container>
-        <p>Above is the list of healthy foods for well being.</p>
-      </Container> */}
     </>
   );
 }
--- learning-fragments/src/components/Item.jsx
+++ learning-fragments/src/components/Item.jsx
@@ -1,16 +1,12 @@
 import styles from "./Item.module.css";
 
-const Item = ({ foodItem }) => {
-  const handleOnClick = (event) => {
-    console.log(event);
-    console.log(`Item being bought: ${foodItem}`);
-  };
+const Item = ({ foodItem, handleBuyButton }) => {
   return (
     <li className={`${styles["item"]} list-group-item`}>
       <span className={styles["item-span"]}>{foodItem}</span>
       <button
         className={`${styles["button"]} btn btn-info`}
-        onClick={(event) => handleOnClick(event)}
+        onClick={handleBuyButton}
       >
         Buy
       </button>
